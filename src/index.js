8edd11200ddb27ab9c2f264eb1a76080df7fbae1
karina_alexandrovna <masalova.karina@mail.ru>
2020-10-16 10:33:00 +0300

    refactor: change add/remove for toggle

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 const kebab = document.querySelector('.kebab-menu__item');
 const menu = document.querySelector('.menu');
-const menuItem = document.querySelector('.common');
 
 kebab.addEventListener('click', (event) => {
     if (!kebab.classList.contains('kebab-menu__item_active')) {
-        kebab.classList.add('kebab-menu__item_active');
-        menu.classList.add('menu_active');
+        kebab.classList.toggle('kebab-menu__item_active');
+        menu.classList.toggle('menu_active');
     } else {
-        kebab.classList.remove('kebab-menu__item_active');
-        menu.classList.remove('menu_active');
+        kebab.classList.toggle('kebab-menu__item_active');
+        menu.classList.toggle('menu_active');
     }
 });
